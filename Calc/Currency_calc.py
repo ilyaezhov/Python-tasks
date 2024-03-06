2698142c3e9b4c49352b52a1a84799b5545d9752
Ilya <ezhovilya3@gmail.com>
2024-03-06 15:35:30 +0300

    Конвертер валют

---
+++ Calc/Currency_calc.py
@@ -0,0 +1,8 @@
+a = int(input('Write the number of dollars:'))
+rubles = 110
+euros = 0.9
+franks = 1.07
+dollars_rubles = a * rubles
+print('Rubles:', dollars_rubles)
+print('euros:', a * euros)
+print('franks:', a * franks)

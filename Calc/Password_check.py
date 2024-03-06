3d547fa0f7c1e164de73e40e49589c3d4f0b251b
Ilya <ezhovilya3@gmail.com>
2024-03-06 16:02:00 +0300

    Проверка пароля

---
+++ Calc/Password_check.py
@@ -0,0 +1,6 @@
+password_1 = (input('Write your password:'))
+password_2 = (input('Write your password again:'))
+if (password_1 == password_2):
+    print('Password accepted')
+else:
+    print('Password not accepted')
